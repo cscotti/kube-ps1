598df8f5123dde7de792fbfd58e1de23d2851960
c.scotti <cscotti@monoprix.fr>
2019-01-13 22:05:37 +0100

    change color when prod

--- .kube-prompt.sh
+++ .kube-prompt.sh
@@ -7,29 +7,29 @@ __parse_git_branch() {
 __kube_ps1()
 {
     # Get current context
-    CONTEXT=$(cat ~/.kube/config | grep "current-context:" --color=NO | sed "s/current-context: //" | sed "s/gke_monoprix-//"| sed "s/_europe.*//")
+    CONTEXT=$(cat ~/.kube/config | grep "current-context:" --color=NO | sed "s/current-context: //" )
 
-    #if [ -n "$CONTEXT" ]; then
-        #if [ "$CONTEXT" = "prod" ]; then
-         #   echo -e "${BRIGHT}${WHITE}${RED_BG}${BLINK}${CONTEXT}${NORMAL}"
-        #else
-          #  echo -e "${YELLOW}${CONTEXT}${NORMAL}"
-       # fi
-    #fi
-    echo ${CONTEXT}
+    if [ -n "$CONTEXT" ]; then
+        if [ "$CONTEXT" = "prod" ]; then
+            echo "${RED}${CONTEXT}${NORMAL}"
+        else
+            echo "${CONTEXT}"
+        fi
+    fi
+    #echo ${CONTEXT}
 }
 
 __gcloud_ps1()
 {
     CONTEXT=$(cat ~/.config/gcloud/active_config)
 
-    #if [ -n "$CONTEXT" ]; then
-     #   if [ "$CONTEXT" = "ninja-prod" ]; then
-      #      echo -e "${BRIGHT}${WHITE}${RED_BG}${BLINK}${CONTEXT}${NORMAL}"
-       # else
-        #    echo -e "${YELLOW}${CONTEXT}${NORMAL}"
-        #fi
-    #fi
-    echo ${CONTEXT}
+    if [ -n "$CONTEXT" ]; then
+        if [ "$CONTEXT" = "ninja-prod" ]; then
+            echo "${RED}${CONTEXT}${NORMAL}"
+        else
+            echo "${CONTEXT}"
+        fi
+    fi
+    #echo ${CONTEXT}
 }
 
